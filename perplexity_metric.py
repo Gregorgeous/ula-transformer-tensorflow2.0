1d56fdd33dabfa80801ae92a1fd7acc0feb32a1d
Grzegorz Rybak <g.r.fisher.pl@gmail.com>
2019-06-09 21:49:58 +0200

    (Major) Created *custom* TF Metric for perplexity.
    - NB. It's *experimental* ! This is my first attempt of creating
    my own custom object in Tensorflow.
    - In TensorFlow 2.0 metrics have a  brand new form of stateful objects,
    and since there's no metric measuring perplexity in TF API so far,
    I sub-classed the Metric object and made a custom one
    using the formula shown by Kirill Mavreshko in his Keras-Transformer:
    https://github.com/kpot/keras-transformer.
    (perplexity is a rather better metric than accuracy for lang gen. model)
    - The metric is expecting y_true and y_pred - that's usual for metrics,
    bear in mind, though, it expects y_pred being in format of logits -
    that's exactly what transformer currently outputs during training loop,
    but bear that in mind if using the Metric for any different scenario.
    - NB the additional argument "sample_weight" is not handled.

---
+++ perplexity_metric.py
@@ -0,0 +1,43 @@
+import tensorflow as tf
+K = tf.keras.backend # Alias to Keras' backend namespace.
+
+class PerplexityMetric(tf.keras.metrics.Metric):
+    """
+    USAGE NOTICE: this metric accepts only logits for now (i.e. same way that in tf.keras.losses.SparseCategoricalCrossentropy from_logits is 'False', here it's enforced to be always True so you need to provide it in such a format)
+    METRIC DESCRIPTION:
+    Popular metric for evaluating language modelling architectures.
+    More info: http://cs224d.stanford.edu/lecture_notes/LectureNotes4.pdf.
+    DISCLAIMER: Original function created by Kirill Mavreshko in https://github.com/kpot/keras-transformer/blob/b9d4e76c535c0c62cadc73e37416e4dc18b635ca/example/run_gpt.py#L106. 
+    My "contribution": I converted Kirill method's logic into a Tensorflow 2.0 way of doing things - this requireD making the metric a fully-fledged object by subclassing the Metric object. 
+    """
+    def __init__(self, name='perplexity', **kwargs):
+      super(PerplexityMetric, self).__init__(name=name, **kwargs)
+      self.cross_entropy = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True, reduction='none')
+      self.perplexity = self.add_weight(name='tp', initializer='zeros')
+
+    # @tf.function
+    def _calculate_loss(self, real, pred):
+      mask = tf.math.logical_not(tf.math.equal(real, 0))
+      loss_ = self.cross_entropy(real, pred)
+      mask = tf.cast(mask, dtype=loss_.dtype)
+      loss_ *= mask
+      test1 = K.mean(loss_, axis=-1)
+      test2 = K.exp(test1)
+      perplexity = K.mean(test2)
+
+      return perplexity 
+
+
+    def update_state(self, y_true, y_pred, sample_weight=None):
+      # TODO:FIXME: handle sample_weight ! 
+      if sample_weight is not None:
+          print("WARNING! Provided 'sample_weight' argument to the perplexity metric. Currently this is not handled and won't do anything differently..")
+      perplexity = self._calculate_loss(y_true, y_pred)
+      self.perplexity.assign_add(perplexity)
+        
+    def result(self):
+      return self.perplexity
+
+    def reset_states(self):
+      # The state of the metric will be reset at the start of each epoch.
+      self.perplexity.assign(0.)
